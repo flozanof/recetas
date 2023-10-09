e4ca499cda42f54f46e1f41664c7f3f2983e4a27
flozano <flozano@dome-consulting.com>
2023-10-09 20:41:16 +0200

    Pruebas deploy vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-//    output: 'export',
-//    basePath: '/gh-pages',
+    //    output: 'export',
+    //    basePath: '/gh-pages',
     //distDir: "build"
+//    images: {
+//        domains: ["images.pexels.com", "miro.medium.com", "www.cnet.com"]
+//    }
 }
 
 module.exports = nextConfig
